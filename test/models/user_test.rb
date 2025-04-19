39905ce22753b37799b81ebfa7f446a1fd7f1848
George Zalepa <george.zalepa@gmail.com>
2025-04-19 10:20:29 -0400

    add user tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,38 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should create a valid user" do
+    user = User.new(email: "test@example.com", password: "password123", password_confirmation: "password123")
+    assert user.valid?
+  end
+
+  test "should not create a user without an email" do
+    user = User.new(password: "password123", password_confirmation: "password123")
+    assert_not user.valid?
+    assert_includes user.errors[:email], "can't be blank"
+  end
+
+  test "should not create a user with invalid email" do
+    user = User.new(email: "invalid_email", password: "password123", password_confirmation: "password123")
+    assert_not user.valid?
+    assert_includes user.errors[:email], "is invalid"
+  end
+
+  test "should not create a user without a password" do
+    user = User.new(email: "test@example.com")
+    assert_not user.valid?
+    assert_includes user.errors[:password], "can't be blank"
+  end
+
+  test "should not create a user with mismatched password confirmation" do
+    user = User.new(email: "test@example.com", password: "password123", password_confirmation: "wrongpassword")
+    assert_not user.valid?
+    assert_includes user.errors[:password_confirmation], "doesn't match Password"
+  end
+
+  test "should not create a user with a short password" do
+    user = User.new(email: "test@example.com", password: "short", password_confirmation: "short")
+    assert_not user.valid?
+    assert_includes user.errors[:password], "is too short (minimum is 6 characters)"
+  end
 end
